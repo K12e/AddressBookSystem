89cfab97900847c725cae97476e9667d4e48af6f
k12e <kalyanirathod2095@gmail.com>
2022-05-24 08:57:58 +0530

    Added: Welcome to address book

---
+++ src/main/java/Contact.java
@@ -0,0 +1,68 @@
+public class Contact {
+    private String Fname;
+    private String Lname;
+    private String address;;
+    private String city;
+    private String state;
+    private String phoneNumber;
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public void setFname(String fname) {
+        Fname = fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "Fname='" + Fname + '\'' +
+                ", Lname='" + Lname + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
